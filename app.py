708e5fb71a536508c9c1583d8f6bc332be573f0d
Maria Luiza <126245983+mluizaguedes@users.noreply.github.com>
2023-05-05 08:30:36 -0300

    arquivo users

--- app.py
+++ app.py
@@ -26,3 +26,38 @@ def home():
 @app.route("/quemsomos")
 def quem_somos():
     return render_template("quemsomos.html")
+
+'''
+@app.route("/contatos")
+def contatos():
+    return render_template("contatos.html")
+
+'''
+
+@app.route('/contatos', methods=['GET', 'POST'])
+def contatos():
+    if request.method== "POST"
+    email= request.form ['email']
+    assunto= request.form ['assunto']
+    descricao= request.form ['descricao']
+
+    cur= mysql.connection.cursor()
+    cur.execute("INSERT INTO contatos(email, assunto, descricao) VALUES (%s, %s, %s)", (email, assunto, descricao))
+
+    mysql.connection.commit() 
+
+    cur.close()
+
+    return 'sucesso'
+return render_template('contatos.html')
+
+@app.route('/users')
+def users():
+    cur = mysql.connection.cursor()
+
+    users = cur.execute("SELECT * FROM contatos")
+
+    if users > 0:
+        userDetails = cur.fetchall()
+
+        return render_template("users.html", userDetails=userDetails)
